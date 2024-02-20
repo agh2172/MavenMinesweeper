83d7eefaac7db4668ada5b7127e18acd4e76f5f0
Axel <agh2172@columbia.edu>
2024-02-20 11:58:08 +0000

    game tests for flag placement addedwq

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,12 +9,19 @@ public class GameTest {
         Assertions.assertEquals(9, minesweeper.getColSize(), "wrong column size");
         Assertions.assertEquals(9, minesweeper.getRowSize(), "wrong row size");
 
-        minesweeper.initialiseBoard(3,3);
-        Assertions.assertFalse(minesweeper.getTile(3,3).isMine(), "initial click cannot be mine");
-
         minesweeper = new Game(2);
         Assertions.assertEquals(16, minesweeper.getColSize(), "wrong column size");
         Assertions.assertEquals(16, minesweeper.getRowSize(), "wrong row size");
+
+        minesweeper.initialiseBoard(3,3);
+        Assertions.assertFalse(minesweeper.getTile(3,3).isMine(), "initial click cannot be mine");
+
+        boolean flagPlaced = minesweeper.placeFlag(15,15, true);
+        Assertions.assertTrue(flagPlaced, "flag not placed successfully");
+        Assertions.assertTrue(minesweeper.getTile(15,15).isFlag(), "should be a flag");
+        boolean badFlagPlaced = minesweeper.placeFlag(3,3, true);
+        Assertions.assertFalse(badFlagPlaced, "flag place here should fail");
+        Assertions.assertFalse(minesweeper.getTile(3,3).isFlag(), "should not be a flag");
 
         minesweeper = new Game(3);
         Assertions.assertEquals(16, minesweeper.getColSize(), "wrong column size");
