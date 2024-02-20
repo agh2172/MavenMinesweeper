02ba6e1df68bed872622d69fb575f1189c5e97dc
Axel <agh2172@columbia.edu>
2024-02-20 11:48:27 +0000

    Added test for difficulty =-1

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,6 +9,9 @@ public class GameTest {
         Assertions.assertEquals(9, minesweeper.getColSize(), "wrong column size");
         Assertions.assertEquals(9, minesweeper.getRowSize(), "wrong row size");
 
+        minesweeper.initialiseBoard(3,3);
+        Assertions.assertFalse(minesweeper.getTile(3,3).isMine(), "initial click cannot be mine");
+
         minesweeper = new Game(2);
         Assertions.assertEquals(16, minesweeper.getColSize(), "wrong column size");
         Assertions.assertEquals(16, minesweeper.getRowSize(), "wrong row size");
@@ -16,5 +19,13 @@ public class GameTest {
         minesweeper = new Game(3);
         Assertions.assertEquals(16, minesweeper.getColSize(), "wrong column size");
         Assertions.assertEquals(30, minesweeper.getRowSize(), "wrong row size");
+
+        minesweeper = new Game(-1);
+        Assertions.assertEquals(5, minesweeper.getColSize(), "wrong column size");
+        Assertions.assertEquals(5, minesweeper.getRowSize(), "wrong row size");
+
+        Assertions.assertFalse(minesweeper.isGameOver(), "gameOver should be false");
+        Assertions.assertFalse(minesweeper.isLost(), "gameLost should be false");
+
     }
 }
