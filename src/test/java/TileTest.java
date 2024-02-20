c6dd7b39db9b34c175462cc1fb3fbc6ecf8791b4
Axel <agh2172@columbia.edu>
2024-02-20 11:24:33 +0000

    More tile tests added

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -7,13 +7,24 @@ public class TileTest {
     public void testTileGetter(){
         Tile temp = new Tile(true, 0, 0);
         Assertions.assertEquals(0, temp.getCol(), "wrong column initialised");
+        temp.setCol(3);
+        Assertions.assertEquals(3, temp.getCol(), "wrong column upon reassignment");
         Assertions.assertEquals(0, temp.getRow(), "wrong row initialised");
+        temp.setRow(3);
+        Assertions.assertEquals(3, temp.getCol(), "wrong row upon reassignment");
         Assertions.assertTrue(temp.isMine(), "mine is not mine");
         temp.setMine(false);
         Assertions.assertFalse(temp.isMine(), "mine set to not mine");
         temp.setFlag(true);
         Assertions.assertTrue(temp.isFlag(), "flag is not flag");
         Assertions.assertEquals("⚑", temp.toString(), "wrong string returned");
+        temp.setFlag(false);
+        temp.setCount(5);
+        Assertions.assertEquals(5, temp.getCount(), "wrong count initialised");
+        temp.setShow(true);
+        Assertions.assertTrue(temp.isShow(), "show is false");
+        Assertions.assertEquals("5", temp.toString(), "wrong string returned");
+
     }
 
 
